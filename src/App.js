97bee8821b9879762f0510453173e911b53005ec
godiarksel <godiarksel@gmail.com>
2023-07-05 21:04:40 +0300

    Add API key url and film search function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import { useEffect } from 'react';
 
+const API_URL = 'http://www.omdbapi.com/?apikey=f3866de6'
 
 const App = () => {
+
+    const movieSearch = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        movieSearch('Tenet');
+    }, []);
+
     return (
 
         <h1>App</h1>
