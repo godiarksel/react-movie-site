fb4e460e7a68d5d2466576112febe489517b576f
godiarksel <godiarksel@gmail.com>
2023-07-17 12:22:24 +0300

    Refactor movie card code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect } from 'react';
-
+import Movies from './movies';
 import './App.css';
 import searchIcon from './search.svg';
 
@@ -44,21 +44,7 @@ const App = () => {
             </div>
 
             <div className='container'>
-                <div className='movie'>
-                    <div>
-                        <p>{oneMovie.Year}</p>
-                    </div>
-                    <div>
-                        <img src={oneMovie.Poster !== 'N/A' ? oneMovie.Poster : 'https://via.placeholder.com/400'}
-                             alt={oneMovie.Title}
-                        />
-                    </div>
-                    <div>
-                        <span>{oneMovie.Type}</span>
-                        <h3>{oneMovie.Title}</h3>
-                    </div>
-                </div>
-                
+                <Movies oneMovie = {oneMovie} />
             </div>
         </div>
 
