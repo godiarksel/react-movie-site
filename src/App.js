0e96c073a41e45a67eeee99b285e5d9f0d558da2
godiarksel <godiarksel@gmail.com>
2023-07-17 11:45:39 +0300

    Add movie card

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import { useEffect } from 'react';
 import './App.css';
 import searchIcon from './search.svg';
 
-
 const API_URL = 'http://www.omdbapi.com/?apikey=f3866de6'
+
+const oneMovie ={
+    "Title": "Tenet",
+    "Year": "2020",
+    "imdbID": "tt6723592",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzU3YWYwNTQtZTdiMC00NjY5LTlmMTMtZDFlYTEyODBjMTk5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -37,7 +44,21 @@ const App = () => {
             </div>
 
             <div className='container'>
-
+                <div className='movie'>
+                    <div>
+                        <p>{oneMovie.Year}</p>
+                    </div>
+                    <div>
+                        <img src={oneMovie.Poster !== 'N/A' ? oneMovie.Poster : 'https://via.placeholder.com/400'}
+                             alt={oneMovie.Title}
+                        />
+                    </div>
+                    <div>
+                        <span>{oneMovie.Type}</span>
+                        <h3>{oneMovie.Title}</h3>
+                    </div>
+                </div>
+                
             </div>
         </div>
 
